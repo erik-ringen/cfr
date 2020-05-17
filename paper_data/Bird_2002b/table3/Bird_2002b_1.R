5a4dca53f9ba56c4be40637bce03342222bbefc3
Your Name <you@example.com>
2020-05-17 19:13:39 -0400

    updated paths and added units to Bird_2002b_table3

--- paper_data/Bird_2002b/table3/Bird_2002b_1.R
+++ paper_data/Bird_2002b/table3/Bird_2002b_1.R
@@ -8,13 +8,13 @@ usePackage("tidyverse")
 
 ##################################
 home <- getwd() # remember home directory to return to
-temp_dir <- "paper_data/Bird_2002b/1" # temporarily set directory
+temp_dir <- "paper_data/Bird_2002b/table3" # temporarily set directory
 setwd(temp_dir)
 
 paper_name <- strsplit(temp_dir, split="/", fixed=T)[[1]][2]
 paper_section <- strsplit(temp_dir, split="/", fixed=T)[[1]][3]
 
-d <- read_csv("Table3.csv")
+d <- read_csv("table3. Reef-flat collecting efficiency.csv")
 
 #### Step 1: Wrangle data ########
 d_wide <- d %>%
@@ -60,7 +60,7 @@ d_fin <- d_wide
 ##################################
 #### Step 3: Add meta-data and additional covariate information
 d_fin$study <- paper_name # paper id
-d_fin$outcome <- paste(d_fin$study, 1:7, sep="_") # 7 outcomes in total, different shellfish
+d_fin$outcome <- paste(d_fin$study, d_fin$`Prey Type`, sep="_") # 7 outcomes in total, different shellfish
 d_fin$id <- NA # study *  outcome * individual, if data are individual rather than group-level
 d_fin$sex <- "both" # "female", "male", or "both"
 d_fin$age <- NA # no mean age given
@@ -69,11 +69,11 @@ d_fin$age_sd <- NA  # only if sd of ages is given
 d_fin$age_lower <- 5 # only if interval ages given
 d_fin$age_upper <- 15 # only if interval ages given
 d_fin$resource <- "shellfish" # what type of foraging resource
-d_fin$timescale <- "hr" # whether the rate is per hour (hr), per day, or other
+d_fin$units <- "kcal/hr" # whether the rate is per hour (hr), per day, or other
 
 ##################################
 #### Step 4: Export outcome csv for further processing 
-d_export <- d_fin %>% ungroup %>% select(study, outcome, id, sex, age, age_error, age_sd, age_lower, age_upper, resource, timescale, lRR_mean, lRR_sd)
+d_export <- d_fin %>% ungroup %>% select(study, outcome, id, sex, age, age_error, age_sd, age_lower, age_upper, resource, units, lRR_mean, lRR_sd)
 
 write_csv(d_export, paste0( paste(paste("data", paper_name, sep="_"),paper_section, sep="_"), ".csv" ))
 

145087ebea3553c3f48d4bb2c64b03e80d354439
Erik Ringen <ringen.erik@gmail.com>
2021-02-26 00:24:56 -0500

    round 2 data entry for Hawkes_1995

--- paper_data_round2/Hawkes_1995/Table_4/Hawkes_1995_Table_4.r
+++ paper_data_round2/Hawkes_1995/Table_4/Hawkes_1995_Table_4.r
@@ -8,7 +8,7 @@ usePackage("tidyverse")
 
 ##################################
 home <- getwd() # remember home directory to return to
-temp_dir <- "paper_data/Hawkes_1995/Table_4" # temporarily set directory
+temp_dir <- "paper_data_round2/Hawkes_1995/Table_4" # temporarily set directory
 setwd(temp_dir)
 
 paper_name <- strsplit(temp_dir, split="/", fixed=T)[[1]][2]

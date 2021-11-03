025ef51e4cb0608c57d7a87022f376450dd7de3e
Erik Ringen <ringen.erik@gmail.com>
2021-11-03 18:46:06 -0400

    hotfix for sex diff preds

--- cfr_functions.R
+++ cfr_functions.R
@@ -12,7 +12,7 @@ m_col <- "orange"
 age_seq <- seq(from=0, to=20, length.out = 21)
 
 #### Convenience function to plot age curves from cfr model ####
-cfr_pred <- function( outcome=NA, male=NA, id=NA, resource=NA, resp="returns", age=14 ) {
+cfr_pred <- function( outcome=NA, male=NA, id=NA, resource=NA, resp="nodim_returns", age=14 ) {
   
   ## female dummy var
   male <- ifelse(is.na(male), 0, male)
@@ -44,23 +44,23 @@ cfr_pred <- function( outcome=NA, male=NA, id=NA, resource=NA, resp="returns", a
   mu_r <- mu_p
   
   ## k
-  k = exp( post$a[,1] + post$a_k[,1]*post$sigma_sex[,1]*female + post$a_k[,2]*post$sigma_sex[,1]*male + outcome_v[,1] + outcome_v[,8]*female + outcome_v[,15]*male + resource_v[,1] + resource_v[,8]*female + resource_v[,15] )
+  k = exp( post$a[,1] + post$a_k[,1]*post$sigma_sex[,1]*female + post$a_k[,2]*post$sigma_sex[,1]*male + outcome_v[,1] + outcome_v[,8]*female + outcome_v[,15]*male + resource_v[,1] + resource_v[,8]*female + resource_v[,15]*male )
   
   ## b
-  b = exp( post$a[,2] + post$a_b[,1]*post$sigma_sex[,2]*female + post$a_b[,2]*post$sigma_sex[,2]*male + outcome_v[,2] + outcome_v[,9]*female + outcome_v[,16]*male + resource_v[,2] + resource_v[,9]*female + resource_v[,16] )
+  b = exp( post$a[,2] + post$a_b[,1]*post$sigma_sex[,2]*female + post$a_b[,2]*post$sigma_sex[,2]*male + outcome_v[,2] + outcome_v[,9]*female + outcome_v[,16]*male + resource_v[,2] + resource_v[,9]*female + resource_v[,16]*male )
   
   ## eta_p
-  eta[,1] = exp( post$a[,3] + post$a_eta[,1,1]*post$sigma_sex[,3]*female + post$a_eta[,2,1]*post$sigma_sex[,3]*male + outcome_v[,3] + outcome_v[,10]*female + outcome_v[,17]*male + resource_v[,3] + resource_v[,10]*female + resource_v[,17] )
+  eta[,1] = exp( post$a[,3] + post$a_eta[,1,1]*post$sigma_sex[,3]*female + post$a_eta[,2,1]*post$sigma_sex[,3]*male + outcome_v[,3] + outcome_v[,10]*female + outcome_v[,17]*male + resource_v[,3] + resource_v[,10]*female + resource_v[,17]*male )
   
   ## eta_r
-  eta[,2] = exp( post$a[,4] + post$a_eta[,1,2]*post$sigma_sex[,4]*female + post$a_eta[,2,2]*post$sigma_sex[,4]*male + outcome_v[,4] + outcome_v[,11]*female + outcome_v[,18]*male + resource_v[,4] + resource_v[,11]*female + resource_v[,18] )
+  eta[,2] = exp( post$a[,4] + post$a_eta[,1,2]*post$sigma_sex[,4]*female + post$a_eta[,2,2]*post$sigma_sex[,4]*male + outcome_v[,4] + outcome_v[,11]*female + outcome_v[,18]*male + resource_v[,4] + resource_v[,11]*female + resource_v[,18]*male )
   
   ## Skill
   for (n in 1:n_preds) S[,n] = ( 1 - exp(-k * (age[n]/20) ))^b
   
-  p = exp( post$a[,5] + post$a_p[,1]*post$sigma_sex[,5]*female + post$a_p[,2]*post$sigma_sex[,5]*male + outcome_v[,5] + outcome_v[,12]*female + outcome_v[,19]*male + resource_v[,5] + resource_v[,12]*female + resource_v[,19] )
+  p = exp( post$a[,5] + post$a_p[,1]*post$sigma_sex[,5]*female + post$a_p[,2]*post$sigma_sex[,5]*male + outcome_v[,5] + outcome_v[,12]*female + outcome_v[,19]*male + resource_v[,5] + resource_v[,12]*female + resource_v[,19]*male )
   
-  alpha = exp( post$a[,6] + post$a_alpha[,1]*post$sigma_sex[,6]*female + post$a_alpha[,2]*post$sigma_sex[,6]*male + outcome_v[,6] + outcome_v[,13]*female + outcome_v[,20]*male + resource_v[,6] + resource_v[,13]*female + resource_v[,20] )
+  alpha = exp( post$a[,6] + post$a_alpha[,1]*post$sigma_sex[,6]*female + post$a_alpha[,2]*post$sigma_sex[,6]*male + outcome_v[,6] + outcome_v[,13]*female + outcome_v[,20]*male + resource_v[,6] + resource_v[,13]*female + resource_v[,20]*male )
   
   for (n in 1:n_preds) {
     mu_p[,n] = (S[,n]^eta[,1]) * p
